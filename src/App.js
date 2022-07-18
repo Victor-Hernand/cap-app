395b1d324a64d72ee0bf916700daab64ea1eeac2
chrisalvarenga <chrisalvarenga19@yahoo.com>
2022-07-17 20:25:34 -0600

    page Reception create

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 
 import './App.css';
-import Recepciones from './pages/Recepciones/Recepciones';
 import NotFound from './pages/NotFound';
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Receptions from './pages/Recepciones/Receptions';
+import ReceptionCreate from './pages/Recepciones/ReceptionCreate';
+
 
 const App = () => {
 
   return (
     <Router>
       <Routes>
-      <Route path="/" element={<Recepciones />}/>
+      <Route path="/" element={<Receptions />}/>
+      <Route path="/recepciones/crear" element={<ReceptionCreate />}/>
       <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
--- src/pages/Recepciones/Receptions.js
+++ src/pages/Recepciones/Receptions.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import BasicLayout from "../../components/BasicLayout";
 import Loading from "../../components/Loading";
 import Pagination from "../../components/Pagination";
 import Search from "../../components/Search";
 import { getClientes, searchReception } from "../../services/recepcionesService";
-import RecepcionesCard from "./RecepcionesCard";
+import RecepcionesCard from "./CardReceptions";
 
-const Recepciones = () => {
+const Receptions = () => {
     const [page, setPage] = useState(0);
     const [total, setTotal] = useState(0);
     const [loading, setLoading] = useState(true);
@@ -73,13 +74,14 @@ const Recepciones = () => {
                 <h1>Recepciones de vehículos</h1>
             </div>
             <div className="pl-3 mb-4">
-                <button type="button" className="py-2 px-4 flex justify-center items-center bg-blue-500 hover:bg-blue-600 text-white w-18 transition ease-in duration-200 text-center text-base font-semibold rounded-md ">
-                    Recepción de vehículo
+            <Link to="/recepciones/crear" className="w-72 py-2 px-4 flex justify-center items-center bg-blue-500 hover:bg-blue-600 text-white w-18 transition ease-in duration-200 text-center text-base font-semibold rounded-md ">
+                Recepción de vehículo
                     <svg width="20" height="20" fill="currentColor" className="ml-2" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
                         <path d="M1408 960V832q0-26-19-45t-45-19h-320V448q0-26-19-45t-45-19H832q-26 0-45 19t-19 45v320H448q-26 0-45 19t-19 45v128q0 26 19 45t45 19h320v320q0 26 19 45t45 19h128q26 0 45-19t19-45v-320h320q26 0 45-19t19-45zm256-544v960q0 119-84.5 203.5T1376 1664H416q-119 0-203.5-84.5T128 1376V416q0-119 84.5-203.5T416 128h960q119 0 203.5 84.5T1664 416z">
                         </path>
                     </svg>
-                </button>
+                
+            </ Link>
             </div>
             <Search onSearch={onSearch} />
             {notFound ? (
@@ -108,4 +110,4 @@ const Recepciones = () => {
     )
 }
 
-export default Recepciones;
+export default Receptions;
