f47771a4eda55141a4a5cd0b411084807c9a9f46
chrisalvarenga <chrisalvarenga19@yahoo.com>
2022-07-18 19:20:32 -0600

    update cards receptions

--- src/components/BrandDropdown.js
+++ src/components/BrandDropdown.js
@@ -27,14 +27,14 @@ const BrandDropdown = (props) => {
 
     return (
 
-        <div className="m-3 flex flex-col items-center">
+        <div className="p-2 flex flex-col items-center">
             <label className="sr-only">Search</label>
             <div className="relative w-full">
                 <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
                     <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path></svg>
                 </div>
                 <input onChange={searchBrand} type="text" id="brand-search" 
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full pl-10 p-2.5 " 
+                    className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500 block w-full pl-10 p-2.5" 
                     placeholder="Buscar marca" required autoComplete="off" />
                 <input type="hidden" id="brand_id" />
             </div>
--- src/pages/Recepciones/ReceptionPage.js
+++ src/pages/Recepciones/ReceptionPage.js
@@ -7,41 +7,58 @@ import ClientDropdown from "../../components/ClientDropdown";
 import MechanicDropdown from "../../components/MechanicDropdown";
 import { postReception } from "../../services/ReceptionService";
 const ReceptionPage = () => {
-    const submitReceptionForm = async() => {
+    const submitReceptionForm = async () => {
         const form = document.getElementById("reception-form");
         const formData = new FormData(form);
-        const response = await postReception(formData);
-        console.log(response)
+        //const response = await postReception(formData);
+        console.log(form);
+        console.log(formData);
+        //console.log(response)
+        
     }
     return (
         <BasicLayout>
             <h2 className="my-4 text-center font-semibold text-xl text-gray-500">Formulario de recepcion</h2>
-            <form method="POST" id="reception-form" onSubmit={() => submitReceptionForm }>
-                <CarPlateSearch />
-                <ClientDropdown />
-                <MechanicDropdown />
-                <BrandDropdown />
-                <ModelDropdown />
-                
-                <div className="flex flex-col items-center p-2">
-                    <label className="sr-only" htmlFor="motor">Motor</label>
-                    <input type="text" id="motor" name="motor" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full pl-10 p-2.5" placeholder="Motor" required />
+            <div className="mt-4 grid grid-cols-1 md:grid-cols-1 sm:grid-cols-1 ">
+                <div className="px-4 pb-6 sm:px-10">
+                    <form method="POST" id="reception-form" onSubmit={() => submitReceptionForm}>
+                        <CarPlateSearch />
+                        <ClientDropdown />
+                        <MechanicDropdown />
+                        <BrandDropdown />
+                        <ModelDropdown />
+
+                        <div className="flex flex-col items-center p-2">
+                            <label className="sr-only" htmlFor="motor">Motor</label>
+                            <input type="text" id="motor" name="motor" className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500 block w-full pl-10 p-2.5" placeholder="Motor" required />
+                        </div>
+                        <div className="flex flex-col items-center p-2">
+                            <label className="sr-only" htmlFor="car_year">Año</label>
+                            <input type="text" id="car_year" name="car_year" className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500 block w-full pl-10 p-2.5" placeholder="Año" required />
+                        </div>
+                        <div className="flex flex-col items-center p-2">
+                            <label className="sr-only" htmlFor="mileage">Kilometraje</label>
+                            <input type="text" id="mileage" name="mileage" className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500 block w-full pl-10 p-2.5" placeholder="Kilometraje" required />
+                        </div>
+                        <div className="flex flex-col items-center p-2">
+                            <label className="sr-only" htmlFor="traction">Tracción</label>
+                            <input type="text" id="traction" name="traction" className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500 block w-full pl-10 p-2.5" placeholder="Traccion" required />
+                        </div>
+                        <div className="flex flex-col items-center p-2">
+                            <label className="sr-only" htmlFor="date">Fecha</label>
+                            <input type="date" id="date" name="date" className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500 block w-full pl-10 p-2.5" placeholder="Traccion" required />
+                        </div>
+                        <div className="pr-2 float-right my-4 ">
+                            <button type="submit" className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
+                                Guardar
+                            </button>
+                        </div>
+
+                    </form>
                 </div>
-                <div className="flex flex-col items-center p-2">
-                    <label className="sr-only" htmlFor="car_year">Año</label>
-                    <input type="text" id="car_year" name="car_year" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full pl-10 p-2.5" placeholder="Año" required />
-                </div>
-                <div className="flex flex-col items-center p-2">
-                    <label className="sr-only" htmlFor="mileage">Kilometraje</label>
-                    <input type="text" id="mileage" name="mileage" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full pl-10 p-2.5" placeholder="Kilometraje" required />
-                </div>
-                <div className="flex flex-col items-center p-2">
-                    <label className="sr-only" htmlFor="traction">Traccion</label>
-                    <input type="text" id="traction" name="traction" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full pl-10 p-2.5" placeholder="Traccion" required />
-                </div>
-            </form>
+            </div>
         </BasicLayout>
-            
+
     )
 }
 
