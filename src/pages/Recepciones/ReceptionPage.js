cecf71406db88e33318485f0ee15d899deb25419
chrisalvarenga <chrisalvarenga19@yahoo.com>
2022-07-22 17:17:48 -0600

    upload multiple files

--- src/pages/Recepciones/ReceptionPage.js
+++ src/pages/Recepciones/ReceptionPage.js
@@ -9,8 +9,11 @@ import MechanicDropdown from "../../components/MechanicDropdown";
 import CategoriesCard from "../../components/CategoriesCard";
 import { getCategories } from "../../services/CategoriesService";
 import { postReception } from "../../services/ReceptionService";
+import Resizer from "react-image-file-resizer";
 const ReceptionPage = () => {
     const [categories, setCategories] = useState();
+    const [img, setImg] = useState(null);
+    const [newImg, setNewImg] = useState(null);
 
     const getAllCategories = async () => {
         try {
@@ -27,15 +30,53 @@ const ReceptionPage = () => {
         e.preventDefault();
         const form = document.getElementById("reception-form");
         const formData = new FormData(form);
-
-        for(const item of formData.entries()){
+        console.log(newImg);
+        formData.delete("files");
+        for (let i = 0; i < newImg.length; i++) {
+            formData.append("files",newImg[i]);
+        }   
+        for (const item of formData.entries()) {
             console.log(item)
         }
-        return
         const response = await postReception(formData);
-        
-        
+        console.log(response);
+
     }
+
+
+    const onFileResize = (event) => {
+        var fileInput = false;
+        let files = event.target.files;
+        let imagenes = [];
+    if (files) {
+        fileInput = true;
+    }
+    if (fileInput) {
+        for (let i = 0; i < files.length; i++) {
+            try {
+                Resizer.imageFileResizer(
+                    files[i],
+                    300,
+                    300,
+                    "JPEG",
+                    100,
+                    0,
+                    (uri) => {
+                        imagenes.push(uri);
+                        console.log(imagenes)
+                        setNewImg(imagenes);
+                    },
+                    "file",
+                    200,
+                    200
+                );
+            } catch (err) {
+                console.log(err);
+            }
+        }
+    }
+    }
+
     return (
         <BasicLayout>
             <h2 className="my-4 text-center font-semibold text-xl text-gray-500">Formulario de recepcion</h2>
@@ -50,19 +91,19 @@ const ReceptionPage = () => {
 
                         <div className="flex flex-col items-center p-2">
                             <label className="sr-only" htmlFor="car_motor">Motor</label>
-                            <input type="text" id="car_motor" name="car_motor" className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500 block w-full pl-10 p-2.5" placeholder="Motor"  />
+                            <input autoComplete="false" type="text" id="car_motor" name="car_motor" className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500 block w-full pl-10 p-2.5" placeholder="Motor" />
                         </div>
                         <div className="flex flex-col items-center p-2">
                             <label className="sr-only" htmlFor="car_year">Año</label>
-                            <input type="text" id="car_year" name="car_year" className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500 block w-full pl-10 p-2.5" placeholder="Año"  />
+                            <input autoComplete="false" type="text" id="car_year" name="car_year" className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500 block w-full pl-10 p-2.5" placeholder="Año" />
                         </div>
                         <div className="flex flex-col items-center p-2">
                             <label className="sr-only" htmlFor="car_mileage">Kilometraje</label>
-                            <input type="text" id="car_mileage" name="car_mileage" className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500 block w-full pl-10 p-2.5" placeholder="Kilometraje"  />
+                            <input autoComplete="false" type="text" id="car_mileage" name="car_mileage" className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500 block w-full pl-10 p-2.5" placeholder="Kilometraje" />
                         </div>
                         <div className="flex flex-col items-center p-2">
                             <label className="sr-only" htmlFor="car_traction">Tracción</label>
-                            <input type="text" id="car_traction" name="car_traction" className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500 block w-full pl-10 p-2.5" placeholder="Traccion"  />
+                            <input autoComplete="false" type="text" id="car_traction" name="car_traction" className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500 block w-full pl-10 p-2.5" placeholder="Traccion" />
                         </div>
                         {/*<div className="flex flex-col items-center p-2">
                             <label className="sr-only" htmlFor="date">Fecha</label>
@@ -70,7 +111,12 @@ const ReceptionPage = () => {
                         </div>*/}
 
                         <CategoriesCard categories={categories} />
-
+                        <div className="p-2 flex">
+                            <div className="mb-3 w-96">
+                                <label htmlFor="formFileMultiple" className="form-label inline-block mb-2 font-semibold text-indigo-600">Agregar Imagenes</label>
+                                <input onChange={onFileResize} name="files" accept="image/jpeg" className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" type="file" id="formFileMultiple" multiple />
+                            </div>
+                        </div>
                         <div className="pr-2 float-right my-4 ">
                             <button type="submit" className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
                                 Guardar
