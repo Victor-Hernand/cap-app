5f79d8e5bd7ffe7a4e63d01b589e2c03523b2d30
chrisalvarenga <chrisalvarenga19@yahoo.com>
2022-07-22 20:51:25 -0600

    create show component

---
+++ src/pages/Recepciones/ShowReception.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import ReceptionPage from "./ReceptionPage";
+
+const ShowReception = () => {
+const [isDisabled, setIsDisabled] = useState(true);
+    return(
+        <div>Show</div>
+    );
+
+}
+
+export default ShowReception;
