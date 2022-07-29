94fd1cc6bd68a83b3434a60a399b6cdeab408fee
chrisalvarenga <chrisalvarenga19@yahoo.com>
2022-07-28 19:08:53 -0600

    view diagnostic

---
+++ src/pages/Diagnostic/CardDiagnostic.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Loading from "../../components/Loading";
+
+const CardDiagnostic = (props) => {
+    const { data, loading } = props;
+
+    return (
+        <>
+            {loading ? (
+                <Loading />
+            ) : (
+                <div className="pl-3 mt-4 grid grid-cols-1 md:grid-cols-3 sm:grid-cols-2 bg-slate-50">
+
+                    {data.map((item, idx) => {
+                        return (
+                            <Link key={idx} to={`/diagnostic/show/${item.id}`}>
+                                <div  className="bg-white max-w-sm rounded overflow-hidden shadow-lg my-2 mr-2">
+                                    <div className="px-6 py-4">
+                                        <div className="mb-2">
+                                            <p><span className="text-lg font-bold"> Cliente: </span>{item.client_name}</p>
+                                            <p><span className="text-lg font-bold">Placa: </span>{item.car_plate}</p>
+                                        </div>
+                                    </div>
+                                    <div className="px-6 pt-4 pb-2">
+                                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Marca: {item.car_brand}</span>
+                                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Modelo: {item.car_model}</span>
+                                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Fecha: {item.exam_date}</span>
+                                    </div>
+                                </div>
+                            </Link>
+
+                        );
+                    })}
+                </div>
+            )}
+        </>
+    );
+}
+
+export default CardDiagnostic;
